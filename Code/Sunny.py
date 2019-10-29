30d4d8b7f1ee556082768aa0a7935fc68e329e3f
2018180011woochan <45459935+2018180011woochan@users.noreply.github.com>
2019-10-29 23:19:03 +0900

    Update Sunny.py

--- Code/Sunny.py
+++ Code/Sunny.py
@@ -1,5 +1,48 @@
 from pico2d import*
 
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SPACE = range(5)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_SPACE): SPACE
+}
+
+class IdleState:
+    @staticmethod
+    def enter(sunny, event):
+        if event == RIGHT_DOWN:
+            sunny.velocity += 1
+            if sunny.x > 570:
+                sunny.x = 570
+        elif event == LEFT_DOWN:
+            sunny.velocity -= 1
+            if sunny.x < 230:
+                sunny.x = 230
+        elif event == RIGHT_UP:
+            sunny.velocity -= 1
+        elif event == LEFT_UP:
+            sunny.velocity += 1
+        if sunny.x > 570:
+            sunny.x = 570
+        elif sunny.x < 230:
+            sunny.x = 230
+
+    @staticmethod
+    def exit(sunny, event):
+        pass
+
+    @staticmethod
+    def do(sunny):
+        sunny.x += sunny.velocity
+        sunny.x = clamp(25, sunny.x, 800 - 25)
+        pass
+
+    @staticmethod
+    def draw(sunny):
+        sunny.image.draw(sunny.x, sunny.y)
 
 class Sunny:
 
@@ -8,26 +51,34 @@ class Sunny:
         self.dir = 1
         self.velocity = 0
         self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
         self.image = load_image('Player_Sunny.png')
 
+    def update_state(self):
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state.enter(self, event)
+
     def handle_event(self, event):
-        if event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
-            self.x += 10
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
-            self.x -= 10
-        if self.x > 570:
-            self.x -= 10
-        elif self.x < 230:
-            self.x += 10
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
 
     def add_event(self, event):
         self.event_que.insert(0, event)
 
     def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state.enter(self, event)
         pass
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.cur_state.draw(self)
 
 
 
